3e606436c226ce0b10ba3ad74b86d8b8a2618227
Surendra Prasad Nishad <surendraprasadnishad123@gmail.com>
2023-04-08 02:31:39 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProjectForm';
+export class AppComponent implements OnInit {
+  constructor(){}
+  //variable array for holding semester and displaying radio button
+  Semesters:string[]=['First','Second','Third','Fourth','Fifth','Sixth','Seventh','Eighth'];
+
+  //varable holding Default gender
+  DefaultGender="Female";
+  
+/*receive local referenedirectlyby using viewchildassign it in  signupForm:oftype(NgForm) and */
+@ViewChild('f') signupForm:NgForm;
+  
+OnSubmit(){
+console.log(this.signupForm);
+}
+  /*secondway receive local from method form ie: (ngSubmit)="OnSubmit(f)" #f="ngForm"
+  OnSubmit(form:NgForm){
+console.log(form);
+  }*/
+  ngOnInit(): void {
+    
+  }
 }
